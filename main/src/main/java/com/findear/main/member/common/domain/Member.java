25153f217210f45ff255983cd1510dcd2c4b1dba
Ehigh <rkd2rb@naver.com>
2024-10-31 17:17:23 +0900

    Refactor: Member 테이블 탈퇴여부 컬럼에 기본값(false)추가

--- main/src/main/java/com/findear/main/member/common/domain/Member.java
+++ main/src/main/java/com/findear/main/member/common/domain/Member.java
@@ -8,6 +8,7 @@ import com.findear.main.board.common.domain.Scrap;
 import com.findear.main.message.common.domain.MessageRoom;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -66,6 +67,7 @@ public class Member {
     private LocalDateTime withdrawalAt;
 
     @Column(nullable = false)
+    @ColumnDefault("0")
     private Boolean withdrawalYn;
 
     private String naverRefreshToken;
