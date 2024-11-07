64e3de37ee427fbecd2887cd3b7d03e1cb956091
Ehigh <rkd2rb@naver.com>
2024-11-07 19:49:10 +0900

    Style: MatchingService 클래스 내 메소드 위치 조정

--- stubBatchServer/src/main/java/com/findear/stubbatchserver/service/MatchingService.java
+++ stubBatchServer/src/main/java/com/findear/stubbatchserver/service/MatchingService.java
@@ -21,9 +21,9 @@ import java.util.*;
 @Service
 @Transactional
 public class MatchingService {
+    private final FindearMatchingRepository findearMatchingRepository;
 
     private final Lost112MatchingRepository lost112MatchingRepository;
-    private final FindearMatchingRepository findearMatchingRepository;
 
     public FindearMatchingListResDto getFindearBestMatching(Long memberId, int page, int size) {
         List<FindearMatching> matchingList = findearMatchingRepository.findAllByLostMemberId(memberId);
@@ -32,23 +32,27 @@ public class MatchingService {
         return getFindearPageResponse(bestMatchings, page, size);
     }
 
-    private FindearMatchingListResDto getFindearPageResponse(List<FindearMatching> bestMatchings, int page, int size) {
-        List<FindearMatching> slicedData = slicePage(bestMatchings, page, size);
-        if (slicedData.isEmpty()) {
-            return new FindearMatchingListResDto(new ArrayList<>(), 0);
-        }
+    public FindearMatchingListResDto getFindearMatchingList(Long lostBoardId, int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<FindearMatching> matchingPage = findearMatchingRepository
+                .findAllByLostBoardIdOrderBySimilarityRateDesc(pageable, lostBoardId);
 
-        List<FindearMatchingDto> pagingResult = slicedData.stream()
-                .map(FindearMatchingDto::of)
-                .toList();
-        return new FindearMatchingListResDto(pagingResult, bestMatchings.size());
+        return findearPageToResponse(matchingPage);
     }
 
-    private <T> List<T> slicePage(List<T> totalList, int page, int size) {
-        int start = (page - 1) * size;
-        int end = Math.min(start + size, totalList.size());
+    public Lost112MatchingListResDto getLost112BestMatching(Long memberId, int page, int size) {
+        List<Lost112Matching> matchingList = lost112MatchingRepository.findAllByLostMemberId(memberId);
+        List<Lost112Matching> bestMatchings = extractLost112Bests(matchingList);
 
-        return start < end ? totalList.subList(start, end) : new ArrayList<>();
+        return getLost112PageResponse(bestMatchings, page, size);
+    }
+
+    public Lost112MatchingListResDto getLost112MatchingList(Long lostBoardId, int page, int size) {
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<Lost112Matching> matchingPage = lost112MatchingRepository
+                .findAllByLostBoardIdOrderBySimilarityRateDesc(pageable, lostBoardId);
+
+        return lost112PageToResponse(matchingPage);
     }
 
     private List<FindearMatching> extractFindearBests(List<FindearMatching> matchingList) {
@@ -67,25 +71,45 @@ public class MatchingService {
         return bestMatchings;
     }
 
+    private FindearMatchingListResDto getFindearPageResponse(List<FindearMatching> bestMatchings, int page, int size) {
+        List<FindearMatching> slicedData = slicePage(bestMatchings, page, size);
+        if (slicedData.isEmpty()) {
+            return new FindearMatchingListResDto(new ArrayList<>(), 0);
+        }
+
+        List<FindearMatchingDto> pagingResult = slicedData.stream()
+                .map(FindearMatchingDto::of)
+                .toList();
+        return new FindearMatchingListResDto(pagingResult, bestMatchings.size());
+    }
+
+    private FindearMatchingListResDto findearPageToResponse(Page<FindearMatching> matchingPage) {
+        List<FindearMatchingDto> dtoList = matchingPage.getContent().stream()
+                .map(FindearMatchingDto::of)
+                .toList();
+        return new FindearMatchingListResDto(dtoList, matchingPage.getTotalElements());
+    }
+
     private FindearMatching getMoreSimilar(FindearMatching matching1, FindearMatching matching2) {
         return matching1.getSimilarityRate() > matching2.getSimilarityRate()
                 ? matching1
                 : matching2;
     }
 
-    public FindearMatchingListResDto getFindearMatchingList(Long lostBoardId, int page, int size) {
-        Pageable pageable = PageRequest.of(page - 1, size);
-        Page<FindearMatching> matchingPage = findearMatchingRepository
-                .findAllByLostBoardIdOrderBySimilarityRateDesc(pageable, lostBoardId);
+    private List<Lost112Matching> extractLost112Bests(List<Lost112Matching> matchingList) {
+        Map<Long, Lost112Matching> bests = new HashMap<>();
 
-        return findearPageToResponse(matchingPage);
-    }
+        for (Lost112Matching matching : matchingList) {
+            Long key = matching.getLostBoardId();
+            bests.put(key, bests.containsKey(key) ?
+                    getMoreSimilar(matching, bests.get(key)) // 기존 값이 있다면, 유사도가 높은 것을 남김
+                    : matching); // 없다면, 현재 값 추가
+        }
 
-    public Lost112MatchingListResDto getLost112BestMatching(Long memberId, int page, int size) {
-        List<Lost112Matching> matchingList = lost112MatchingRepository.findAllByLostMemberId(memberId);
-        List<Lost112Matching> bestMatchings = extractLost112Bests(matchingList);
-
-        return getLost112PageResponse(bestMatchings, page, size);
+        List<Lost112Matching> bestMatchings = new ArrayList<>(bests.values());
+        // 유사도 순 정렬
+        bestMatchings.sort((m1, m2) -> m1.getSimilarityRate() > m2.getSimilarityRate() ? -1 : 1);
+        return bestMatchings;
     }
 
     private Lost112MatchingListResDto getLost112PageResponse(List<Lost112Matching> bestMatchings, int page, int size) {
@@ -100,45 +124,23 @@ public class MatchingService {
         return new Lost112MatchingListResDto(pagingResult, bestMatchings.size());
     }
 
-    private List<Lost112Matching> extractLost112Bests(List<Lost112Matching> matchingList) {
-        Map<Long, Lost112Matching> bests = new HashMap<>();
-
-        for (Lost112Matching matching : matchingList) {
-            Long key = matching.getLostBoardId();
-            if (bests.containsKey(key)) {
-                bests.compute(key, (k, oldValue) ->
-                        oldValue.getSimilarityRate() > matching.getSimilarityRate()
-                                ? oldValue : matching);
-            } else {
-                bests.put(key, matching);
-            }
-        }
-
-        List<Lost112Matching> bestMatchings = new ArrayList<>(bests.values());
-        // 유사도 순 정렬
-        bestMatchings.sort((m1, m2) -> m1.getSimilarityRate() > m2.getSimilarityRate() ? -1 : 1);
-        return bestMatchings;
-    }
-
-    public Lost112MatchingListResDto getLost112MatchingList(Long lostBoardId, int page, int size) {
-        Pageable pageable = PageRequest.of(page - 1, size);
-        Page<Lost112Matching> matchingPage = lost112MatchingRepository
-                .findAllByLostBoardIdOrderBySimilarityRateDesc(pageable, lostBoardId);
-
-        return lost112PageToResponse(matchingPage);
-    }
-
-    private FindearMatchingListResDto findearPageToResponse(Page<FindearMatching> matchingPage) {
-        List<FindearMatchingDto> dtoList = matchingPage.getContent().stream()
-                .map(FindearMatchingDto::of)
-                .toList();
-        return new FindearMatchingListResDto(dtoList, matchingPage.getTotalElements());
-    }
-
     private Lost112MatchingListResDto lost112PageToResponse(Page<Lost112Matching> matchingPage) {
         List<Lost112MatchingDto> dtoList = matchingPage.getContent().stream()
                 .map(Lost112MatchingDto::of)
                 .toList();
         return new Lost112MatchingListResDto(dtoList, matchingPage.getTotalElements());
+    }
+
+    private Lost112Matching getMoreSimilar(Lost112Matching matching1, Lost112Matching matching2) {
+        return matching1.getSimilarityRate() > matching2.getSimilarityRate()
+                ? matching1
+                : matching2;
+    }
+
+    private <T> List<T> slicePage(List<T> totalList, int page, int size) {
+        int start = (page - 1) * size;
+        int end = Math.min(start + size, totalList.size());
+
+        return start < end ? totalList.subList(start, end) : new ArrayList<>();
     }
 }
