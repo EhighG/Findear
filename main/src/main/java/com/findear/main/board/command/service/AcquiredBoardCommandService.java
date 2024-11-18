ea11651ddba0bba2cfe6ba0370fe9c5c7df0db16
Ehigh <rkd2rb@naver.com>
2024-11-19 01:28:25 +0900

    Refactor: AcquiredBoardCommandService 추상화

---
+++ main/src/main/java/com/findear/main/board/command/service/AcquiredBoardCommandService.java
@@ -0,0 +1,17 @@
+package com.findear.main.board.command.service;
+
+import com.findear.main.board.command.dto.GiveBackReqDto;
+import com.findear.main.board.command.dto.ModifyAcquiredBoardReqDto;
+import com.findear.main.board.command.dto.PostAcquiredBoardReqDto;
+import com.findear.main.board.common.dto.ScrapListResDto;
+
+public interface AcquiredBoardCommandService {
+    Long register(PostAcquiredBoardReqDto postAcquiredBoardReqDto);
+    Long modify(ModifyAcquiredBoardReqDto modifyReqDto);
+    void remove(Long boardId, Long memberId);
+    void giveBack(GiveBackReqDto giveBackReqDto);
+    void cancelGiveBack(Long managerId, Long boardId);
+    void scrap(Long memberId, String boardId, Boolean isFindear);
+    ScrapListResDto findScrapList(Long memberId);
+    void cancelScrap(Long memberId, String boardId, Boolean isFindear);
+}
--- main/src/main/java/com/findear/main/board/command/service/AcquiredBoardCommandServiceImpl.java
+++ main/src/main/java/com/findear/main/board/command/service/AcquiredBoardCommandServiceImpl.java
@@ -29,7 +29,7 @@ import java.util.Map;
 @Transactional
 @RequiredArgsConstructor
 @Service
-public class AcquiredBoardCommandServiceImpl {
+public class AcquiredBoardCommandServiceImpl implements AcquiredBoardCommandService {
 
     private final AcquiredBoardCommandRepository acquiredBoardCommandRepository;
     private final AcquiredBoardQueryRepository acquiredBoardQueryRepository;
