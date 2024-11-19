434f72a18565e83844a2b4ffa2c163e738813894
Ehigh <rkd2rb@naver.com>
2024-11-20 01:42:01 +0900

    Test: LostBoard findById() 성공 케이스 추가

---
+++ main/src/test/java/com/findear/main/board/query/service/LostBoardQueryServiceTest.java
@@ -0,0 +1,91 @@
+package com.findear.main.board.query.service;
+
+import com.findear.main.board.common.domain.Board;
+import com.findear.main.board.common.domain.ImgFile;
+import com.findear.main.board.common.domain.LostBoard;
+import com.findear.main.board.query.dto.LostBoardDetailResDto;
+import com.findear.main.board.query.repository.LostBoardQueryRepository;
+import com.findear.main.member.common.domain.Member;
+import com.findear.main.member.common.domain.Role;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+class LostBoardQueryServiceTest {
+
+    private LostBoardQueryService lostBoardQueryService;
+
+    @Mock
+    private LostBoardQueryRepository lostBoardQueryRepository;
+    private AutoCloseable closeable;
+
+    @BeforeEach
+    void setup() {
+        closeable = MockitoAnnotations.openMocks(this);
+        lostBoardQueryService = new LostBoardQueryServiceImpl(lostBoardQueryRepository);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        closeable.close();
+    }
+
+    @DisplayName("분실물 ID로 조회")
+    @Test
+    void findById() {
+        // given
+        doReturn(foundLostBoardOptional()).when(lostBoardQueryRepository)
+                .findById(any(Long.class));
+        
+        // when
+        LostBoardDetailResDto foundLostBoard = lostBoardQueryService.findById(1L);
+
+        // then
+        assertThat(foundLostBoard.getLostBoardId()).isNotNull();
+        assertThat(foundLostBoard.getBoard().getId()).isNotNull();
+        assertThat(foundLostBoard.getBoard().getMember().getMemberId()).isNotNull();
+    }
+
+    private Optional<LostBoard> foundLostBoardOptional() {
+        Member writer = Member.builder()
+                .id(1L)
+                .phoneNumber("testPhoneNumber")
+                .role(Role.NORMAL)
+                .build();
+        Board foundBoard = Board.builder()
+                .id(1L)
+                .isLost(true)
+                .member(writer)
+                .productName("testProductName")
+                .categoryName("testCategoryName")
+                .imgFileList(List.of(new ImgFile(1L, "testImgUrl1")))
+                .color("testColor")
+                .registeredAt(LocalDateTime.now())
+                .build();
+
+        return Optional.of(
+                LostBoard.builder()
+                        .id(1L)
+                        .board(foundBoard)
+                        .lostAt(LocalDate.now())
+                        .suspiciousPlace("testPlace")
+                        .xPos(9.99f)
+                        .yPos(9.99f)
+                        .build()
+        );
+    }
+}
