3145733036d251e4bd605ee2e5c29d89e18f030f
Ehigh <rkd2rb@naver.com>
2024-11-20 18:16:21 +0900

    Test: 문제 상황 캡쳐-테스트용으로 LostBoard 1개만 조회하도록 수정
    Related to #9

--- main/src/main/java/com/findear/main/board/query/repository/LostBoardQueryRepository.java
+++ main/src/main/java/com/findear/main/board/query/repository/LostBoardQueryRepository.java
@@ -1,8 +1,6 @@
 package com.findear.main.board.query.repository;
 
 import com.findear.main.board.common.domain.LostBoard;
-import com.findear.main.board.common.domain.Board;
-import com.findear.main.board.common.domain.LostBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -20,7 +18,7 @@ public interface LostBoardQueryRepository extends JpaRepository<LostBoard, Long>
     Optional<LostBoard> findById(Long lostBoardId);
 
     // delete_yn 기본값 적용 시 where 조건 바꾸기
-    @Query("select lb from LostBoard lb join fetch lb.board left join fetch lb.board.imgFileList where lb.board.deleteYn = false")
+    @Query("select lb from LostBoard lb join fetch lb.board left join fetch lb.board.imgFileList where lb.board.deleteYn = false and lb.id = 1")
     List<LostBoard> findAll();
     @Query("select lb from LostBoard lb join fetch lb.board left join fetch lb.board.imgFileList where lb.board.deleteYn = false order by lb.lostAt")
     List<LostBoard> findAllOrderByLostAt();
