c3780f7ef1bb5c7acc10a8e7f04d3772a1997244
Ehigh <rkd2rb@naver.com>
2024-11-26 01:30:05 +0900

    Refactor: count쿼리에서 최소한의 join만 수행하도록 변경
    lostBoard와 join이 필요한 상황에는, board쪽에 lostBoard에 관한 FK가
    없으므로 select(lostBoard)~.join(board)로 처리

--- main/src/main/java/com/findear/main/common/config/QuerydslConfig.java
+++ main/src/main/java/com/findear/main/common/config/QuerydslConfig.java
@@ -3,7 +3,6 @@ package com.findear.main.common.config;
 import com.blazebit.persistence.querydsl.JPQLNextTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
