3492b42ce759e695a7a1269f09a064562f43b3be
Ehigh <rkd2rb@naver.com>
2024-11-26 01:30:05 +0900

    Refactor: count쿼리에서 최소한의 join만 수행하도록 변경
    lostBoard와 join이 필요한 상황에는, board쪽에 lostBoard에 관한 FK가
    없으므로 select(lostBoard)~.join(board)로 처리
    
    Related to #10

--- main/src/main/java/com/findear/main/board/query/repository/LostBoardQueryCustomRepositoryImpl.java
+++ main/src/main/java/com/findear/main/board/query/repository/LostBoardQueryCustomRepositoryImpl.java
@@ -31,7 +31,7 @@ public class LostBoardQueryCustomRepositoryImpl implements LostBoardQueryCustomR
     // TODO: 쿼리 성능 개선 필요
     @Override
     public Page<LostBoardListResDto> findAllWithDtoForm(FindAllLostBoardReqDto findAllReq, Pageable pageable) {
-        List<LostBoardListResDto> contents = jpaQueryFactory.select(Projections.fields(LostBoardListResDto.class,
+        List<LostBoardListResDto> contents = jpaQueryFactory.select(Projections.constructor(LostBoardListResDto.class,
                         lostBoard.id,
                         board.id,
                         board.productName,
@@ -48,26 +48,42 @@ public class LostBoardQueryCustomRepositoryImpl implements LostBoardQueryCustomR
                 .where(board.deleteYn.not(),
                         memberIdEq(findAllReq.getMemberId()),
                         categoryLike(findAllReq.getCategory()),
-                        dateBetween(findAllReq.getSDate(), findAllReq.getEDate()),
+                        lostAtBetween(findAllReq.getSDate(), findAllReq.getEDate()),
                         productNameLike(findAllReq.getKeyword()))
                 .orderBy(createOrder(findAllReq.getSortBy(), findAllReq.isDesc()))
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        JPAQuery<Long> countQuery = jpaQueryFactory
-                .select(lostBoard.count())
-                .from(lostBoard)
-                .join(lostBoard.board, board)
-                .join(board.member, member)
-                .leftJoin(board.imgFileList, imgFile)
-                .where(board.deleteYn.not(),
-                        memberIdEq(findAllReq.getMemberId()),
-                        categoryLike(findAllReq.getCategory()),
-                        dateBetween(findAllReq.getSDate(), findAllReq.getEDate()),
-                        productNameLike(findAllReq.getKeyword()));
-
+        JPAQuery<Long> countQuery = createCountQuery(findAllReq);
         return PageableExecutionUtils.getPage(contents, pageable, countQuery::fetchOne);
+    }
+
+    private JPAQuery<Long> createCountQuery(FindAllLostBoardReqDto findAllReq) {
+        JPAQuery<Long> countQuery = reflectLostAt(findAllReq.getSDate(), findAllReq.getEDate());
+
+        countQuery.where(categoryLike(findAllReq.getCategory()),
+                productNameLike(findAllReq.getKeyword()),
+                board.deleteYn.not());
+
+        if (findAllReq.getMemberId() != null) {
+            countQuery.where(board.member.id.eq(findAllReq.getMemberId()));
+        }
+        return countQuery;
+    }
+
+    private JPAQuery<Long> reflectLostAt(LocalDate sDate, LocalDate eDate) {
+        if (sDate == null) { // lostBoard 필요 없음
+            return jpaQueryFactory
+                    .select(board.count())
+                    .from(board);
+        } else { // lostBoard와 조인 필요 -> lostBoard에 board를 조인해야 한다.
+            return jpaQueryFactory
+                    .select(lostBoard.count())
+                    .from(lostBoard)
+                    .join(lostBoard.board, board)
+                    .where(lostBoard.lostAt.between(sDate, eDate));
+        }
     }
 
 
@@ -80,7 +96,7 @@ public class LostBoardQueryCustomRepositoryImpl implements LostBoardQueryCustomR
     }
 
     // 둘 다 있거나, 둘 다 없다. (한쪽만 없을 땐 서비스 단에서 넣어준다.)
-    private BooleanExpression dateBetween(LocalDate sDate, LocalDate eDate) {
+    private BooleanExpression lostAtBetween(LocalDate sDate, LocalDate eDate) {
         if (sDate == null && eDate == null) {
             return null;
         }
